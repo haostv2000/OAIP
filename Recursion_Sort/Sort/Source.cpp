6c00e3d066781d787b12227ddf7d5871827d72c8
Sand-by <Doroshinsky@mail.ru>
2022-01-07 14:12:16 +0300

    Добавил примеры рекурсивной сортировки

---
+++ Recursion_Sort/Sort/Source.cpp
@@ -0,0 +1,63 @@
+//Быстрая(рекурсивная) сортировка одномерного массива
+#include <iostream>
+#include<ctime>
+using namespace std;
+const int n = 7;
+int l, r;
+//функция сортировки
+void quicksort(int* mas, int l, int r)
+{
+	int mid, count;
+	int i = l, j = r;
+	mid = mas[(i + j) / 2]; //вычисление опорного элемента
+	do
+	{
+		while (mas[i] < mid) i++;
+		while (mas[j] > mid) j--;
+		if (i <= j) //перестановка элементов
+		{
+			count = mas[i];
+			mas[i] = mas[j];
+			mas[j] = count;
+			i++;
+			j--;
+		}
+	} while (i < j);
+	if (l < j) quicksort(mas, l, j);
+	if (i < r) quicksort(mas, i, r);
+}
+//главная функция
+void main()
+{
+	setlocale(LC_ALL, "Rus");
+	int* A = new int[n];
+	srand(time(NULL));
+	cout << "Исходный массив: ";
+	for (int i = 0; i < n; i++)
+	{
+		A[i] = rand() % 10;
+		cout << A[i] << " ";
+	}
+	l = 0; r = n - 1;
+	quicksort(A, l, r);
+	cout << endl << "Результирующий массив: ";
+	for (int i = 0; i < n; i++) cout << A[i] << " ";
+	delete[]A;
+	system("pause");
+}
+
+
+
+//const int N = 7;
+//int mas[N]{ 4,9,7,6,2,3,8 };
+
+//i                 j
+//4, 9, 7, 6, 2, 3, 8
+//l        ^        r
+//
+//         ij
+//4, 3, 2, 6, 7, 9, 8
+//l        ^        r
+
+//p = (l + r) / 2 = (0 + 6) / 2 = 3;
+
---
+++ Recursion_Sort/Struct_sort/Source.cpp
@@ -0,0 +1,87 @@
+//Быстрая сортировка структуры
+#include<iostream>
+#include<string>
+#include<iomanip>
+#include<Windows.h>
+using namespace std;
+struct strc {
+	string FIO;
+	int ngr;
+	int otc[4];
+	double sb;
+} mstud[25];
+void vvod_strc(strc mstud[], int n) {
+	for (int i = 0; i < n; i++)
+	{
+		cout << "Введите ФИО:";
+		getline(cin, mstud[i].FIO);
+
+		cout << "Введите номер группы:";
+		cin >> mstud[i].ngr;
+		cout << "Введите 4 оценки: " << endl;
+		mstud[i].sb = 0;
+		for (int j = 0; j < 4; j++)
+		{
+			cin >> mstud[i].otc[j];
+			mstud[i].sb += mstud[i].otc[j] / 4.;
+		}
+		cin.ignore();
+		cout << endl;
+	}
+}
+void vivod_strc(strc s[], int n) {
+	cout << "FiO " << setw(28) << " NGR " << setw(8) << " SB " << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << s[i].FIO << setw(10)
+			<< s[i].ngr << setw(10)
+			<< s[i].sb << endl;
+	}
+}
+void quick_sort(strc a[], int l, int r) {
+	int i = l, j = r;
+	double mid = a[(i + j) / 2].sb;
+	double sb_temp;
+	int ngr_temps;
+	string tempFIO;
+	do {
+		while (a[i].sb < mid) i++;
+		while (a[j].sb > mid) j--;
+		if (i <= j) {
+			sb_temp = a[i].sb;
+			a[i].sb = a[j].sb;
+			a[j].sb = sb_temp;
+
+			ngr_temps = a[i].ngr;
+			a[i].ngr = a[j].ngr;
+			a[j].ngr = sb_temp;
+
+			tempFIO = a[i].FIO;
+			a[i].FIO = a[j].FIO;
+			a[j].FIO = tempFIO;
+
+			i++;
+			j--;
+		}
+	} while (i < j);
+	if (l < j) quick_sort(a, l, j);
+	if (i < r) quick_sort(a, i, r);
+}
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	srand(time(NULL));
+	int nst;
+	cout << "Введите количество студентов:";
+	cin >> nst;
+	cin.ignore();
+	vvod_strc(mstud, nst);
+	vivod_strc(mstud, nst);
+	cout << endl;
+	cout << "________________________________________" << endl;
+	int l = 0;
+	int r = nst - 1;
+	quick_sort(mstud, l, r);
+	vivod_strc(mstud, nst);
+	system("pause");
+}
