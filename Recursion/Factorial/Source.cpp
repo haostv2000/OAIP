a49b5ba94e4945ca7fa970c0d0128b12f5b3fb02
doroshinsky <doroshinsky@mail.ru>
2022-01-11 15:58:38 +0300

    gh

--- Recursion/Factorial/Source.cpp
+++ Recursion/Factorial/Source.cpp
@@ -1,14 +1,14 @@
 //Рекурсивное нахождение факториала
 #include<iostream>
 using namespace std;
-int factorial(int n);
+int factorial(int);//ПРОТОТИП-->.h
 int main() {
 	int n = 5;
 	int y = factorial(n);
 	cout << n << "! = " << y << endl;
 	system("pause");
 }
-int factorial(int n) {
+int factorial(int n) {//-->.cpp
 	int t;
 	if (n <= 1)
 		t = 1;
